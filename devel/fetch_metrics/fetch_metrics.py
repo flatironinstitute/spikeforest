2ddf35b01e10bef7fcc5759c819693d772f8bb0a
Jeff Soules <jsoules@flatironinstitute.org>
2021-04-02 15:38:14 -0400

    Improve CLI, reorg, and remove global args variable.

--- devel/fetch_metrics/fetch_metrics.py
+++ devel/fetch_metrics/fetch_metrics.py
@@ -22,15 +22,12 @@ class ArgsDict(TypedDict):
     recordingset: str
     outfile: str
     workercount: int
-    job_cache: str
+    job_cache: Union[str, None]
     use_container: bool
-    use_singularity: bool
     use_slurm: bool
     slurm_max_jobs_per_alloc: int
     slurm_max_simultaneous_allocs: int
-    _slurm_command: str
-
-args: Union[ArgsDict, None] = None
+    slurm_command: str
 
 RECORDING_URI_KEY = 'recordingUri'
 GROUND_TRUTH_URI_KEY = 'sortingTrueUri'
@@ -52,24 +49,7 @@ QUALITY_METRICS = [
     "d_prime"
 ]
 
-
-
 def init_args():
-    args: ArgsDict = {
-        'verbose': 0,
-        'test': 0,
-        'sortingsfile': '',
-        'recordingset': '',
-        'outfile': '',
-        'workercount': 0,
-        'job_cache': 'default-job-cache',
-        'use_container': False,
-        'use_singularity': False,
-        'use_slurm': False,
-        'slurm_max_jobs_per_alloc': 6,
-        'slurm_max_simultaneous_allocs': 5,
-        '_slurm_command': ""
-    }
     parser = argparse.ArgumentParser(description="Compute ground-truth comparisons and quality metrics for SpikeForest records.")
     parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument('--test', '-t', action='store', type=int, default=0,
@@ -86,17 +66,17 @@ def init_args():
     parser.add_argument('--workercount', '-w', action='store', type=int, default=4,
         help="If set, determines the number of worker threads for a parallel job handler. Ignored if using slurm.")
     parser.add_argument('--job_cache', action='store', type=str, default='default-job-cache',
-        help="If set, indicates the feed name for the job cache feed. If set to '', job caching is turned off.")
+        help="If set, indicates the feed name for the job cache feed.")
+    parser.add_argument('--no_job_cache', action='store_true', default=False,
+        help="If set, job cache will not be used, and any value for --job_cache will be ignored.")
     parser.add_argument('--use_container', '-C', action='store_true', default=False,
         help='If set, hither calls will use containerization. If unset, containerization may still be used if ' +
-        'environment variable HITHER_USE_CONTAINER is set to "1" or "TRUE". Note that --use_singularity implies --use_container.')
-    parser.add_argument('--use_singularity', action='store_true', default=False,
-        help='If set, hither calls will use Singularity (rather than Docker) for containerization. Automatically implies --use_container. ' +
-        'If not set, Singularity will still be used if the HITHER_USE_SINGULARITY environment variable is set to 1 or TRUE.')
+        'environment variable HITHER_USE_CONTAINER is set to "1" or "TRUE".')
+    parser.add_argument('--no_container', action='store_true', default=False,
+        help='Override HITHER_USE_CONTAINER environment variable to suppress container use. Ignored if --use_container is set.')
     parser.add_argument('--use_slurm', action='store_true', default=False,
         help='If set, this script will use a SlurmJobHandler and attempt to run jobs on the configured cluster. The exact ' +
-        'call used by the slurm job handler to acquire resources can be customized with command-line arguments, or ' +
-        'set explicitly by the HITHER_SRUN_COMMAND environment variable.')
+        'call used by the slurm job handler to acquire resources can be customized with command-line arguments.')
     parser.add_argument('--slurm_partition', action='store', type=str, default="CCM",
         help='If set, slurm will use the specified text as a partition name to request. Note that slurm must be explicitly ' +
         'requested with the --use_slurm flag; if it is not, this value is ignored.')
@@ -110,47 +90,61 @@ def init_args():
     parser.add_argument('--check_config', action='store_true', default=False,
         help='Debugging tool. If set, program will simply quit with a description of the parsed configuration.')
     parsed = parser.parse_args()
-    # We would very much like to avoid this, unfortunately the argsparse module and the typing module don't play
-    # at all nicely with each other. For a more robust script, we'd want to try a different solution,
+    return parsed
+
+def init_configuration():
+    args: ArgsDict = {
+        'verbose': 0,
+        'test': 0,
+        'sortingsfile': '',
+        'recordingset': '',
+        'outfile': '',
+        'workercount': 0,
+        'job_cache': 'default-job-cache',
+        'use_container': False,
+        'use_slurm': False,
+        'slurm_max_jobs_per_alloc': 6,
+        'slurm_max_simultaneous_allocs': 5,
+        'slurm_command': ""
+    }
+    parsed = init_args()
+    # We would very much like to avoid this manual copying, unfortunately the argsparse module and the typing module
+    # don't play at all nicely with each other. For a more robust script, we'd want to try a different solution,
     # maybe the Tap/Typed Argument Parser module
-    args['verbose'] = parsed.verbose
+    args['verbose'] = parsed.verbose or 0
     args['test'] = parsed.test
     args['sortingsfile'] = parsed.sortingsfile
     args['recordingset'] = parsed.recordingset
     args['outfile'] = parsed.outfile
     args['workercount'] = max(parsed.workercount, 1)
-    args['job_cache'] = parsed.job_cache
-    args['use_singularity'] = parsed.use_singularity or os.getenv('HITHER_USE_SINGULARITY') in ['TRUE', '1']
-    args['use_container'] = parsed.use_container or os.getenv('HITHER_USE_CONTAINER') in ['TRUE', '1'] or args['use_singularity']
+    args['job_cache'] = None if parsed.no_job_cache else parsed.job_cache
+    args['use_container'] = parsed.use_container or ((not parsed.no_container) and os.getenv('HITHER_USE_CONTAINER') in ['TRUE', '1'])
     args['use_slurm'] = parsed.use_slurm
     args['slurm_max_jobs_per_alloc'] = parsed.slurm_jobs_per_allocation
     args['slurm_max_simultaneous_allocs'] = parsed.slurm_max_simultaneous_allocations
     # example srun_command: srun --exclusive -n 1 -p <partition>
-    args['_slurm_command'] = f"srun -n 1 -p {parsed.slurm_partition} {'--exclusive' if not parsed.slurm_share else ''}"
-    # if not parsed.slurm_share: args['_slurm_command'] += ' --exclusive'
-    if os.getenv('HITHER_SRUN_COMMAND') and os.getenv('HITHER_SRUN_COMMAND') is not None:
-        args['_slurm_command'] = os.getenv('HITHER_SRUN_COMMAND') or '' # or '' convinces linter that the envvar isn't null
+    args['slurm_command'] = f"srun -n 1 -p {parsed.slurm_partition} {'--exclusive' if not parsed.slurm_accept_shared_nodes else ''}"
     if parsed.outfile is not None and parsed.outfile != '' and os.path.exists(parsed.outfile) and parsed.outfile != "/dev/null":
         raise Exception('Error: Requested to write to an existing output file. Aborting to avoid overwriting file.')
     if(parsed.check_config):
         print(f"""Received the following environment vars:
             HITHER_USE_CONTAINER: {os.getenv('HITHER_USE_CONTAINER')}
             HITHER_USE_SINGULARITY: {os.getenv('HITHER_USE_SINGULARITY')}
-            HITHER_SRUN_COMMAND: {os.getenv('HITHER_SRUN_COMMAND')}
         """)
-        print(f"""\n\tFinal configuration:
-        {args}
-        """)
+        print(f"\n\tFinal configuration:\n{json.dumps(args, indent=4)}")
         exit()
+    # configure verbosity for the run
+    print_per_verbose.__dict__['verbosity_level'] = args['verbose']
     return args
 
 def print_per_verbose(lvl: int, msg: str):
-    if args and (args['verbose'] < lvl): return
+    # verbosity_level is a static value, initialized from command-line argument at setup time in init_configuration().
+    if (print_per_verbose.verbosity_level < lvl): return
     tabs = max(0, lvl - 1)
     print("\t" * tabs + msg)
 
-def load_sortings() -> List[Dict[str, Any]]:
-    hydrated_sortings = kp.load_json(args['sortingsfile'])
+def load_sortings(sortingsfile: str) -> List[Dict[str, Any]]:
+    hydrated_sortings = kp.load_json(sortingsfile)
     assert hydrated_sortings is not None
     return cast(List[Dict[str, Any]], hydrated_sortings)
 
@@ -268,7 +262,7 @@ def make_comparison_entry(sorting_record, quality_metric_job, gt_comparison_job)
 def extract_sorting_reference_name(comparison_object) -> str:
     return f"({comparison_object['studyName']} {comparison_object['recordingName']} {comparison_object['sorterName']})"
 
-def output_results(comparison_list):
+def output_results(comparison_list, outfile):
     for comparison_object in comparison_list:
         quality_job = comparison_object['quality_metric']
         ground_truth_comparison_job = comparison_object['ground_truth_comparison']
@@ -277,66 +271,53 @@ def output_results(comparison_list):
             comparison_object['ground_truth_comparison'] = ground_truth_comparison_job.result.return_value
             continue
         if quality_job.status == 'error':
-            # This should no longer happen
             print(f"WARNING: quality job for {extract_sorting_reference_name(comparison_object)} had an error.")
         elif quality_job.status != 'finished':
             print(f"WARNING: unfinished non-errored quality job in {extract_sorting_reference_name(comparison_object)}--possible hither error")
         if ground_truth_comparison_job.status == 'error':
-            # This should no longer happen
             print(f"WARNING: comparison job for {extract_sorting_reference_name(comparison_object)} had an error.")
         elif ground_truth_comparison_job.status != 'finished':
             print(f"WARNING: unfinished non-errored comparison job in {extract_sorting_reference_name(comparison_object)}--possible hither error")
             
-        # Replace problematic jobs with an error message (since the job itself cannot be serialized)
+        # Replace problematic jobs with an error message (since we can't serialize the job itself)
         comparison_object['quality_metric'] = f"Job status: {quality_job.status}"
         comparison_object['ground_truth_comparison'] = f"Job status: {ground_truth_comparison_job.status}"
 
-    if args['outfile'] is not None and args['outfile'] != '':
-        with open(args['outfile'], 'x') as f:
+    if outfile is not None and outfile != '':
+        with open(outfile, 'x') as f:
             print(json.dumps(comparison_list, indent=4), file=f)
     else:
         print(f"Results:\n{json.dumps(comparison_list, indent=4)}")
 
 def main():
-    print(f"\t\tScript execution beginning at {time.ctime()}")
-    start_time = time.time()
-    global args
-    args = init_args()
-
+    args = init_configuration()
     use_container = args['use_container']
-    if args['use_singularity']:
-        # Need to be sure that the environment variable is set, since hither will be looking for it.
-        os.environ['HITHER_USE_SINGULARITY'] = '1'
-        print(f"Current state of HITHER_USE_SINGULARITY env var: {os.getenv('HITHER_USE_SINGULARITY')}")
-        exit()
-
-    if use_container:
-        print_per_verbose(1, f"Using {'Singularity' if args['use_singularity'] else 'Docker'} containers.")
-    else:
-        print_per_verbose(1, "Running without containers.")
-
     if args['test'] != 0: print(f"\tRunning in TEST MODE--Execution will stop after processing {args['test']} sortings!\n")
-    count = 0
-    sortings = load_sortings()
-    comparison_list = []
-
+    sortings = load_sortings(args['sortingsfile'])
     if args['recordingset'] is not None and args['recordingset'] != '':
         sortings = [s for s in sortings if s['studyName'] == args['recordingset']]
 
-    # Define job cache and (parallel) job handler
-    jc = None if args['job_cache'] == '' else hi.JobCache(feed_name=args['job_cache'])
+    if use_container:
+        print_per_verbose(1, f"Using {'Singularity' if os.getenv('use_singularity') else 'Docker'} containers.")
+    else:
+        print_per_verbose(1, "Running without containers.")
 
-    # Set up the job handler
+    # Define job cache and job handler
+    jc = None if args['job_cache'] == None else hi.JobCache(feed_name=args['job_cache'])
     if args['use_slurm']:
         jh = hi.SlurmJobHandler(
             num_jobs_per_allocation=args['slurm_max_jobs_per_alloc'],
             max_simultaneous_allocations=args['slurm_max_simultaneous_allocs'],
-            srun_command=args['_slurm_command']
+            srun_command=args['slurm_command']
         )
     else:
         jh = hi.ParallelJobHandler(num_workers=args['workercount'])
 
     try:
+        print(f"\t\tScript execution beginning at {time.ctime()}")
+        count = 0
+        comparison_list = []
+        start_time = time.time()
         with hi.Config(job_cache=jc, job_handler=jh, use_container=use_container):
             for sorting_record in sortings:
                 print_per_verbose(2, f"Creating job-pair {count + 1} ({extract_sorting_reference_name(sorting_record)})")
@@ -348,7 +329,7 @@ def main():
     finally:
         jh.cleanup()
 
-    output_results(comparison_list)
+    output_results(comparison_list, args['outfile'])
     print(f"\n\n\t\tElapsed time: {time.time() - start_time:.3f} sec")
     print(f"\t\tScript execution complete at {time.ctime()}")
 
