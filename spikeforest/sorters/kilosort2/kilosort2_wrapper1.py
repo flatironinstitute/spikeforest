2dab9c59093aafd9f4ce27ca621cc9c93bc0bac4
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-13 10:41:24 -0400

    ks2 wrapper

--- spikeforest/sorters/kilosort2/kilosort2_wrapper1.py
+++ spikeforest/sorters/kilosort2/kilosort2_wrapper1.py
@@ -42,53 +42,58 @@ def kilosort2_wrapper1(
     import labbox_ephys as le
     import spikesorters as ss
 
-    ######################################################################################################################################################
-    # Make sure kilosort is installed, compiled and at the right commit
-    kilosort2_path = os.getenv('KILOSORT2_PATH', None)
-    if not kilosort2_path:
-        raise Exception(f'Environment variable not set: KILOSORT2_PATH')
-    if not os.path.isdir(kilosort2_path):
-        raise Exception(f'Not a directory: {kilosort2_path}')
-    if os.getenv('HITHER_IN_CONTAINER') == '1':
-        with kp.TemporaryDirectory() as tmpdir:
-            kp.ShellScript(f'''
-            cd {kilosort2_path}/CUDA
-            mexGPUall
-            ''').write(f'{tmpdir}/compile_ks.m')
-            compile_script = kp.ShellScript(f'''
-            #!/bin/bash
-            set -e
-
-            cd {tmpdir}
-            matlab -batch compile_ks
-            ''')
-            compile_script.start()
-            retval = compile_script.wait()
-            assert (retval == 0)
-    mex_paths = [f'{kilosort2_path}/CUDA/{fname}' for fname in ['mexClustering2.mexa64', 'mexDistances2.mexa64', 'mexFilterPCs.mexa64', 'mexGetSpikes2.mexa64', 'mexMPnu8.mexa64']] # sampling of some of the files
-    for mex_path in mex_paths:
-        if not os.path.isfile(mex_path):
-            raise Exception(f'Kilosort2 not compiled. File not found: {mex_path}')
-    scr = kp.ShellScript(f'''
-    #!/bin/bash
-    set -e
-    cd {kilosort2_path}
-    if [ `git rev-parse HEAD` == "{expected_kilosort2_commit}" ]; then
-        # at the expected commit
-        exit 0
-    fi
-    exit 1
-    ''')
-    scr.start()
-    if scr.wait() != 0:
-        raise Exception(f'Git repo not at the expected commit: {expected_kilosort2_commit}')
-    ######################################################################################################################################################
-
-    recording = le.LabboxEphysRecordingExtractor(recording_object)
-    
-    # Sorting
-    print('Sorting...')
     with kp.TemporaryDirectory(prefix='tmp_kilosort2') as tmpdir:
+        ######################################################################################################################################################
+        # Make sure kilosort is installed, compiled and at the right commit
+        if os.getenv('HITHER_IN_CONTAINER') == '1':
+                kp.ShellScript(f'''
+                cd {tmpdir}/Kilosort2/CUDA
+                mexGPUall
+                ''').write(f'{tmpdir}/compile_ks.m')
+
+                compile_script = kp.ShellScript(f'''
+                #!/bin/bash
+                set -e
+
+                # copy the source code over to the working directory so we don't have permissions issues during compilation
+                cp -r /src/Kilosort2 {tmpdir}/
+
+                cd {tmpdir}
+                matlab -batch compile_ks
+                ''')
+                compile_script.start()
+                retval = compile_script.wait()
+                assert (retval == 0)
+                os.environ['KILOSORT2_PATH'] = f'{tmpdir}/Kilosort2'
+        kilosort2_path = os.getenv('KILOSORT2_PATH', None)
+        if not kilosort2_path:
+            raise Exception(f'Environment variable not set: KILOSORT2_PATH')
+        if not os.path.isdir(kilosort2_path):
+            raise Exception(f'Not a directory: {kilosort2_path}')
+
+        mex_paths = [f'{kilosort2_path}/CUDA/{fname}' for fname in ['mexClustering2.mexa64', 'mexDistances2.mexa64', 'mexFilterPCs.mexa64', 'mexGetSpikes2.mexa64', 'mexMPnu8.mexa64']] # sampling of some of the files
+        for mex_path in mex_paths:
+            if not os.path.isfile(mex_path):
+                raise Exception(f'Kilosort2 not compiled. File not found: {mex_path}')
+        scr = kp.ShellScript(f'''
+        #!/bin/bash
+        set -e
+        cd {kilosort2_path}
+        if [ `git rev-parse HEAD` == "{expected_kilosort2_commit}" ]; then
+            # at the expected commit
+            exit 0
+        fi
+        exit 1
+        ''')
+        scr.start()
+        if scr.wait() != 0:
+            raise Exception(f'Git repo not at the expected commit: {expected_kilosort2_commit}')
+        ######################################################################################################################################################
+
+        recording = le.LabboxEphysRecordingExtractor(recording_object)
+        
+        # Sorting
+        print('Sorting...')
         sorter = ss.Kilosort2Sorter(
             recording=recording,
             output_folder=f'{tmpdir}/working',
