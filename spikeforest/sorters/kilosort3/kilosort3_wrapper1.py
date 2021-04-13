5039954da7c10cca4e6e7b7831cfcc82702f9388
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-13 10:08:21 -0400

    kilosort3 wrapper

--- spikeforest/sorters/kilosort3/kilosort3_wrapper1.py
+++ spikeforest/sorters/kilosort3/kilosort3_wrapper1.py
@@ -42,22 +42,21 @@ def kilosort3_wrapper1(
     import labbox_ephys as le
     import spikesorters as ss
 
-    ######################################################################################################################################################
-    # Make sure kilosort is installed, compiled and at the right commit
-    kilosort3_path = os.getenv('KILOSORT3_PATH', None)
-    if not kilosort3_path:
-        raise Exception(f'Environment variable not set: KILOSORT3_PATH')
-    if not os.path.isdir(kilosort3_path):
-        raise Exception(f'Not a directory: {kilosort3_path}')
-    if os.getenv('HITHER_IN_CONTAINER') == '1':
-        with kp.TemporaryDirectory() as tmpdir:
+    with kp.TemporaryDirectory(prefix='tmp_kilosort3') as tmpdir:
+        ######################################################################################################################################################
+        # Make sure kilosort is installed, compiled and at the right commit
+        if os.getenv('HITHER_IN_CONTAINER') == '1':
             kp.ShellScript(f'''
-            cd {kilosort3_path}/CUDA
+            cd {tmpdir}/Kilosort3/CUDA
             mexGPUall
             ''').write(f'{tmpdir}/compile_ks.m')
+
             compile_script = kp.ShellScript(f'''
             #!/bin/bash
             set -e
+
+            # copy the source code over to the working directory so we don't have permissions issues during compilation
+            cp -r /src/Kilosort3 {tmpdir}/
 
             cd {tmpdir}
             matlab -batch compile_ks
@@ -65,30 +64,36 @@ def kilosort3_wrapper1(
             compile_script.start()
             retval = compile_script.wait()
             assert (retval == 0)
-    mex_paths = [f'{kilosort3_path}/CUDA/{fname}' for fname in ['mexClustering2.mexa64', 'mexDistances2.mexa64', 'mexFilterPCs.mexa64', 'mexGetSpikes2.mexa64']] # sampling of some of the files
-    for mex_path in mex_paths:
-        if not os.path.isfile(mex_path):
-            raise Exception(f'Kilosort3 not compiled. File not found: {mex_path}')
-    scr = kp.ShellScript(f'''
-    #!/bin/bash
-    set -e
-    cd {kilosort3_path}
-    if [ `git rev-parse HEAD` == "{expected_kilosort3_commit}" ]; then
-        # at the expected commit
-        exit 0
-    fi
-    exit 1
-    ''')
-    scr.start()
-    if scr.wait() != 0:
-        raise Exception(f'Git repo not at the expected commit: {expected_kilosort3_commit}')
-    ######################################################################################################################################################
+            os.environ['KILOSORT3_PATH'] = f'{tmpdir}/Kilosort3'
+        kilosort3_path = os.getenv('KILOSORT3_PATH', None)
+        if not kilosort3_path:
+            raise Exception(f'Environment variable not set: KILOSORT3_PATH')
+        if not os.path.isdir(kilosort3_path):
+            raise Exception(f'Not a directory: {kilosort3_path}')
+        
+        mex_paths = [f'{kilosort3_path}/CUDA/{fname}' for fname in ['mexClustering2.mexa64', 'mexDistances2.mexa64', 'mexFilterPCs.mexa64', 'mexGetSpikes2.mexa64']] # sampling of some of the files
+        for mex_path in mex_paths:
+            if not os.path.isfile(mex_path):
+                raise Exception(f'Kilosort3 not compiled. File not found: {mex_path}')
+        scr = kp.ShellScript(f'''
+        #!/bin/bash
+        set -e
+        cd {kilosort3_path}
+        if [ `git rev-parse HEAD` == "{expected_kilosort3_commit}" ]; then
+            # at the expected commit
+            exit 0
+        fi
+        exit 1
+        ''')
+        scr.start()
+        if scr.wait() != 0:
+            raise Exception(f'Git repo not at the expected commit: {expected_kilosort3_commit}')
+        ######################################################################################################################################################
 
-    recording = le.LabboxEphysRecordingExtractor(recording_object)
+        recording = le.LabboxEphysRecordingExtractor(recording_object)
     
     # Sorting
-    print('Sorting...')
-    with kp.TemporaryDirectory(prefix='tmp_kilosort3') as tmpdir:
+        print('Sorting...')
         sorter = ss.Kilosort3Sorter(
             recording=recording,
             output_folder=f'{tmpdir}/working',
