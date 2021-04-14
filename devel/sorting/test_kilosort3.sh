8398db50590744f8ee69def03192c9a27c1b302a
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-14 08:26:11 -0400

    test ks3

---
+++ devel/sorting/test_kilosort3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# uncomment this if you want to keep all temporary files for debugging purposes
+# export KACHERY_P2P_KEEP_TEMP_FILES=1
+
+# Use singularity rather than docker
+export HITHER_USE_SINGULARITY=1
+
+# Use a matlab license server
+export HITHER_MATLAB_MLM_LICENSE_FILE=$MLM_LICENSE_FILE
+
+./test_kilosort3.py
