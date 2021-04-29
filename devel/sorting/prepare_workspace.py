782e77f45ca071dd6548c0208260171b1bd3e896
Jeff Soules <jsoules@flatironinstitute.org>
2021-04-29 12:59:38 -0400

    Add dry-run flag to prepare_workspace.py. Some bug fixes.

--- devel/sorting/prepare_workspace.py
+++ devel/sorting/prepare_workspace.py
@@ -28,6 +28,7 @@ class RecordingSet(NamedTuple):
 class Params(NamedTuple):
     workspace: str
     sortings:  Any
+    dry_run:   bool
 
 def init() -> Params:
     description = "Convert a sortings.json file into a populated Labbox workspace."
@@ -39,42 +40,48 @@ def init() -> Params:
     parser.add_argument('--sortings-file-kachery-uri', '-k', action='store', default=None,
         help="kachery URI for sortings file. Exactly one of --sortings-file and --sortings-file-kachery-uri "
            + "should be set.")
+    parser.add_argument('--dry-run', action='store_true', default=False,
+        help="If set, script will only parse the input files and display the workspace commands it would have run.")
     parsed = parser.parse_args()
 
     workspace = parsed.workspace_uri
     if (not workspace):
-        # TODO: Create workspace if one is not specified
-        raise NotImplementedError("Workspace creation is not yet implemented.")
+        if parsed.dry_run:
+            workspace = 'Dry run, no workspace created'
+        else:
+            # TODO: Create workspace if one is not specified
+            raise NotImplementedError("Workspace creation is not yet implemented.")
     # TODO: Check if workspace value is valid?
     if ((parsed.sortings_file is None and parsed.sortings_file_kachery_uri is None)
-         or (parsed.sortings_File is not None and parsed.sortings_file_kachery_uri is not None)):
+         or (parsed.sortings_file is not None and parsed.sortings_file_kachery_uri is not None)):
         raise Exception("Exactly one of sortings_file and sortings_file_kachery_uri must be set.")
     if parsed.sortings_file is not None:
         with open(parsed.sortings_file) as fp:
             sortings = json.load(fp)
     else:
         sortings = kp.load_json(parsed.sortings_file_kachery_uri)
-    return Params(workspace, sortings)
+    return Params(workspace, sortings, parsed.dry_run)
 
 def parse_sortings(sortings: Any) -> List[RecordingSet]:
     recording_records: Dict[str, Any] = {}
     for s in sortings:
-        if s.recordingName not in recording_records:
-            recording_records[s.recordingName] = RecordingSet(
-                study_set_label  = s.studyName,
-                recording_name   = s.recordingName,
-                recording_uri    = s.recordingUri,
-                sorting_true_uri = s.sortingTrueUri,
+        if s['recordingName'] not in recording_records:
+            recording_records[s['recordingName']] = RecordingSet(
+                study_set_label  = s['studyName'],
+                recording_name   = s['recordingName'],
+                recording_uri    = s['recordingUri'],
+                sorting_true_uri = s['sortingTrueUri'],
                 sortings         = []
             )
-        recording_records[s.recordingName].sortings.append(SortingTuple(
-            uri   = s.firings,
-            label = f"{s.sorterName}--{s.recordingName}"
+        if 'firings' not in s: continue # when the underlying job errored
+        recording_records[s['recordingName']].sortings.append(SortingTuple(
+            uri   = s['firings'],
+            label = f"{s['sorterName']}--{s['recordingName']}"
         ))
     return [recording_records[k] for k in recording_records.keys()]
 
 def main():
-    (workspace_uri, sortings_json) = init()
+    (workspace_uri, sortings_json, dry_run) = init()
     workspace = le.load_workspace(workspace_uri)
     recording_sets = parse_sortings(sortings_json)
     for r in recording_sets:
@@ -82,10 +89,21 @@ def main():
         recording_label = r.study_set_label + "\\" + r.recording_name
         # TODO: SAMPLE RATE -- assumed 30k
         sorting_true = le.LabboxEphysSortingExtractor(r.sorting_true_uri, samplerate=30000)
-        R_id = workspace.add_recording(recording=recording, label=recording_label)
-        GT_id = workspace.add_sorting(sorting=sorting_true, recording_id=R_id, label=TRUE_SORT_LABEL)
-        s_ids = [workspace.sadd_sorting(sorting=s.uri, recording_id=R_id, label=s.label) for s in r.sortings]
-        # TODO: Something useful with these returned ids? Error check?
+        if dry_run:
+            print(f"Adding {recording.get_num_channels()}-channel recording with label {recording_label}")
+            print(f"Adding GT at {len(sorting_true.get_unit_ids())} units with label {TRUE_SORT_LABEL}")
+            for s in r.sortings:
+                sorting = le.LabboxEphysSortingExtractor(s.uri, samplerate=30000)
+                print(f"Adding sorting {len(sorting.get_unit_ids())} with label {s.label}")
+        else:
+            R_id = workspace.add_recording(recording=recording, label=recording_label)
+            GT_id = workspace.add_sorting(sorting=sorting_true, recording_id=R_id, label=TRUE_SORT_LABEL)
+            s_ids = [workspace.add_sorting(
+                        sorting=le.LabboxEphysSortingExtractor(s.uri, samplerate=30000),
+                        recording_id=R_id,
+                        label=s.label)
+                     for s in r.sortings]
+            # TODO: Something useful with these returned ids? Error check?
     print(f"Loaded {len(recording_sets)} recording sets to workspace {workspace.get_uri}")
 
 if __name__ == "__main__":
