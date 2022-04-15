82f2f7155557f3d8f610d2184b3d838fdf66a6de
Jeremy Magland <jmagland@flatironinstitute.org>
2022-04-15 10:07:58 -0400

    update recordings uri

--- spikeforest/load_spikeforest_recordings/load_spikeforest_recordings.py
+++ spikeforest/load_spikeforest_recordings/load_spikeforest_recordings.py
@@ -1,8 +1,8 @@
 import kachery_cloud as kcl
 from .SFRecording import SFRecording
 
-
-default_uri = 'ipfs://bafkreiantrq7v7tboepsqlvvzqytmvfqe5nrlsyy2g2ztf6xubqlxtz7ae?spikeforest-recordings.json'
+# prepared via: https://github.com/scratchrealm/prepare-spikeforest-ipfs
+default_uri = 'ipfs://bafkreiharnfwm5ntcui4rsex4zkvxfjbytodkserudpvfsnsx5us7tciuq?spikeforest-recordings.json'
 
 def load_spikeforest_recordings(uri: str=default_uri):
     x = kcl.load_json(uri)
--- spikeforest/load_spikeforest_sorting_outputs/load_spikeforest_sorting_outputs.py
+++ spikeforest/load_spikeforest_sorting_outputs/load_spikeforest_sorting_outputs.py
@@ -1,7 +1,7 @@
 import kachery_cloud as kcl
 from .SFSortingOutput import SFSortingOutput
 
-
+# prepared via: https://github.com/scratchrealm/prepare-spikeforest-ipfs
 default_uri = 'ipfs://bafkreiaeqg3vtca6c6j2xbsbppcnfrkm2w46jgc3bfbny6as6mxiy3jine?spikeforest-sorting-outputs.json'
 
 def load_spikeforest_sorting_outputs(uri: str=default_uri):
