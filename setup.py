63a700e59c633699166b2f792dae540edfa9096c
Jeremy Magland <jmagland@flatironinstitute.org>
2022-04-15 10:13:13 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     scripts=[],
     install_requires=[
         'kachery-client>=1.2.0',
-        'kachery-cloud>=0.1.3',
+        'kachery-cloud>=0.1.4',
         'sortingview>=0.7.3',
         'spikeinterface>=0.93.0'
     ]
