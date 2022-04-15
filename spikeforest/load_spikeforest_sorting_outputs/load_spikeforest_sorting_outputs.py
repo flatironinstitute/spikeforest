9004779e808c54249c769e7ef6f1a492289dd12f
Jeremy Magland <jmagland@flatironinstitute.org>
2022-04-15 11:01:36 -0400

    update sorting outputs index

--- spikeforest/load_spikeforest_sorting_outputs/load_spikeforest_sorting_outputs.py
+++ spikeforest/load_spikeforest_sorting_outputs/load_spikeforest_sorting_outputs.py
@@ -2,7 +2,7 @@ import kachery_cloud as kcl
 from .SFSortingOutput import SFSortingOutput
 
 # prepared via: https://github.com/scratchrealm/prepare-spikeforest-ipfs
-default_uri = 'ipfs://bafkreiaeqg3vtca6c6j2xbsbppcnfrkm2w46jgc3bfbny6as6mxiy3jine?spikeforest-sorting-outputs.json'
+default_uri = 'ipfs://bafkreigfiekbk3kghib25l5j2piebrizhjdoittbgvlexgamzjzrp2el54?spikeforest-sorting-outputs.json'
 
 def load_spikeforest_sorting_outputs(uri: str=default_uri):
     x = kcl.load_json(uri)
