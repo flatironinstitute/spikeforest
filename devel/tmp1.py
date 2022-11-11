a7894ed706d37e1e0cf4703b4df42a5bfff239e2
Jeremy Magland <jeremy.magland@gmail.com>
2022-11-11 14:18:09 -0500

    add paired_boyden/kampff/english recordings

--- devel/tmp1.py
+++ devel/tmp1.py
@@ -2,7 +2,7 @@ import kachery_cloud as kcl
 
 x = kcl.load_json('ipfs://bafybeibiqb7znwr7vaidri2dicqtfx6zd7u3szj6swg3iuhrozzvxtqd7u?studysets.json')
 
-studyset = [s for s in x['StudySets'] if s['name'] == 'SYNTH_MONOTRODE'][0]
+studyset = [s for s in x['StudySets'] if s['name'] == 'PAIRED_ENGLISH'][0]
 
 studies = studyset['studies']
 for s in studies:
